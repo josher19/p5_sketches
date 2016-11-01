5bae23c54e1e2a1e1cfd0322767e8e8183c7b67a
Emily Xie <emilyxxie@gmail.com>
2016-11-01 19:50:07 -0400

    Prim's maze completed.

--- sketches/08_prims_maze.js
+++ sketches/08_prims_maze.js
@@ -1,25 +1,31 @@
-spacing   = 0;
-lineWidth = 3;
-totalRows = 0;
-totalColumns = 0;
+var spacing   = 0;
+var lineWidth = 7;
+var lineWidthOffset = 3.25;
+var totalRows = 0;
 
-frontier = {}
-grid = []
+var totalColumns = 0;
+var frontier = {}
+var grid = []
+
+var colors;
+var totalColors = 360;
+var cellColor = 0;
 
 function setup() {
-  createCanvas(1001, 701);
-  // to accomodate for cut off pixel at the end of the maze
-  spacing = (height - 1) / 20;
+  createCanvas(window.innerWidth, window.innerHeight);
+  colors = Array.apply(null, Array(totalColors)).map(function(_, num) {
+    return color('hsl(' + num + ', 40%, 50%)');
+  });
+  spacing = (height) / 60;
   makeGrid();
+  strokeWeight(lineWidth);
   markCell(0, 0);
   expandFrontier(0, 0);
-  frameRate(60);
+  // frameRate(60);
 }
 
 function draw() {
-  // draw from the frontier dictionary, mark as
-  // cell that has already been used up
-  // then remove cell from the dictionary
+  strokeWeight(lineWidth);
   frontier_keys = Object.keys(frontier);
   var pick = Math.round(
     random(frontier_keys.length - 1)
@@ -31,73 +37,74 @@ function draw() {
 
   // mark frontier cell as used
   markCell(row, col);
+  // then tear down wall a random wall adjacent to any given marked neighbor
   removeWall(row, col);
   expandFrontier(row, col);
-
   if (frontier_keys.length == 1) {
     noLoop();
   }
-
-  // # maybe try not to favor top or bottom....
-  // # get all cells to the top, left, bottom, right
-  // # if any of these are marked as "x"
-  // # then we can draw a huge line between the two of them
-  // # if cell to the t, left, bottom, right is marked as "x"
-  // # then make a big line between the two of them
-
-  // markCell();
-  // must do only two things:
-  // 1) mark cell
-  // 1) put this cell into already-explored
-  // 2) add all of it's neighbors into the frontier
-  // 3) take next cell randomly from the frontier, and delete it from the array
-
 }
 
 function expandFrontier(row, col) {
   newFrontier = getAdjacentCells(row, col, 'number');
   newFrontier.forEach(function(coord) {
-    row = coord[0];
-    col = coord[1];
+    var row = coord[0], col = coord[1];
     cell = grid[row][col];
     if (!(cell in frontier)) {
       frontier[cell] = [row, col];
-      fill(20, 50, 50);
+      stroke(20);
+      fill(70);
       rect(col * spacing, row * spacing, spacing, spacing);
     }
   });
 }
 
-// function addFrontier(row, col) {
-//   if (row <= totalRows - 1 && row >= 0 && col <= totalColumns - 1 && col >= 0) {
-//     if (grid[row][col] === true) {
-//       return;
-//     }
-//     cell = grid[row][col];
-//     if (!(cell in frontier)) {
-//       frontier[cell] = [row, col];
-//       fill(20, 50, 50);
-//       rect(col * spacing, row * spacing, spacing, spacing);
-//     }
-//   }
-// }
-
 function markCell(row, col) {
-  stroke(0);
-  fill(255);
+  stroke(20);
+  fill(colors[cellColor]);
   grid[row][col] = true;
-  // mark cell itself
   rect(col * spacing, row * spacing, spacing, spacing);
-  // then tear down wall adjacent to any given neighbor
-  // that is marked as true.
-  text(grid[row][col], 20 + (row * spacing), 20 +  (col * spacing));
+  if (frameCount % 7 == 0) {
+    cellColor ++;
+    cellColor = cellColor % totalColors;
+  }
+
 }
 
 function removeWall(row, col) {
-  // get all neighbors marked with "True"
-  console.log(getAdjacentCells(row, col, 'boolean'));
-  // debugger;
-  // select random neighbor to bridge
+  // get all neighbors marked neighbors
+  ac = getAdjacentCells(row, col, 'boolean');
+  pick = ac[Math.round(random(ac.length - 1))];
+  var ac_row = pick[0], ac_col = pick[1];
+  cell = grid[ac_row][ac_col];
+
+  if (ac_col < col) {
+    // adjacent to the left
+    col = (col * spacing) - lineWidth;
+    row = row * spacing + lineWidth - lineWidthOffset;
+    w   = lineWidth * 2;
+    h   = spacing - lineWidth
+  } else if (ac_col > col) {
+    // adjacent to the right
+    col = ((col + 1) * spacing) - lineWidth;
+    row = (row * spacing) + lineWidth - lineWidthOffset;
+    w   = lineWidth * 2;
+    h   = spacing - lineWidth;
+  } else if (ac_row < row) {
+    // adjacent above
+    col = (col * spacing) + lineWidth - lineWidthOffset;
+    row = (row * spacing) - lineWidth;
+    w   = spacing - lineWidth;
+    h   = lineWidth * 2;
+  } else {
+    // adjacent below
+    col = (col * spacing) + lineWidth - lineWidthOffset;
+    row = ((row + 1) * spacing) - lineWidth;
+    w   = spacing - lineWidth;
+    h   = lineWidth * 2;
+  }
+  noStroke();
+  rect(col, row, w, h);
 }
 
 function getAdjacentCells(row, col, markedAs) {
@@ -107,40 +114,25 @@ function getAdjacentCells(row, col, markedAs) {
     [row + 1, col],
     [row - 1, col]
   ];
-  adjacentCellsF = adjacentCells.filter(function(coord) {
-    row = coord[0];
-    col = coord[1];
+  adjacentCells = adjacentCells.filter(function(coord) {
+    var row = coord[0], col = coord[1];
     if (row <= totalRows - 1 && row >= 0 && col <= totalColumns - 1 && col >= 0) {
-      console.log(markedAs);
-      // debugger;
       return typeof grid[row][col] === markedAs;
     }
   });
-  return adjacentCellsF;
-  // below works. would rather filter.
-  // var filtered_cells = [];
-  // adjacentCells.forEach(function(cell){
-  //   row = cell[0];
-  //   col = cell[1];
-  //   if (row <= totalRows - 1 && row >= 0 && col <= totalColumns - 1 && col >= 0) {
-  //     if (grid[row][col] === markedAs) {
-  //       filtered_cells.push([row, col]);
-  //     };
-  //   }
-  // });
-  // return filtered_cells;
+  return adjacentCells;
 }
 
 function makeGrid() {
-  strokeWeight(3);
+  stroke(48);
   var counter = 0;
-  var row = 0;
-  var col = 0;
+  var row = 0, col = 0;
   for (var y = 0; y <= height; y += spacing) {
     grid.push([])
     for (var x = 0; x <= width; x += spacing) {
+      strokeWeight(2);
       grid[row].push(col);
-      fill(50);
+      fill(35);
       rect(x, y, spacing, spacing);
       col++;
     }
@@ -149,3 +141,4 @@ function makeGrid() {
   totalRows = grid.length;
   totalColumns = grid[0].length;
 }
+
