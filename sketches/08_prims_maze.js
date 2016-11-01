f9fad89903a5dead5567abc9f66f6c8381e79f80
Emily Xie <emilyxxie@gmail.com>
2016-11-01 00:27:39 -0400

    prims maze mostly done - bug to be fixed

--- sketches/08_prims_maze.js
+++ sketches/08_prims_maze.js
@@ -5,11 +5,11 @@ totalColumns = 0;
 
 
 // delete from frontier when a frontier cell has been discovered
-frontier = []
+frontier = {}
 grid = []
 
 function setup() {
-  createCanvas(901, 701);
+  createCanvas(1001, 701);
   // to accomodate for cut off pixel at the end of the maze
   spacing = (height - 1) / 20;
   makeGrid();
@@ -19,17 +19,25 @@ function setup() {
 }
 
 function draw() {
+  // draw from the frontier array, mark as
+  // cell that has already been used up
+  // remove cell from the frontier dictionary
   frontier_keys = Object.keys(frontier);
   var pick = Math.round(
     random(frontier_keys.length - 1)
   );
   cell = frontier_keys[pick];
-  delete frontier[pick];
   row  = frontier[cell][0];
   col  = frontier[cell][1];
+  delete frontier[cell];
+
   markCell(row, col);
   expandFrontier(row, col);
 
+  if (frontier_keys.length == 1) {
+    console.log("stopped");
+    noLoop();
+  }
 
   // # maybe try not to favor top or bottom....
   // # get all cells to the top, left, bottom, right
@@ -48,10 +56,12 @@ function draw() {
 }
 
 function markCell(row, col) {
-  grid[row][col] = 'X';
-  // draw here
+  stroke(0);
   fill(255);
+  grid[row][col] = true;
   rect(row * spacing, col * spacing, spacing, spacing);
+  // debug
+  // text(grid[row][col], 20 + (row * spacing), 20 +  (col * spacing));
 }
 
 function expandFrontier(row, col) {
@@ -63,7 +73,10 @@ function expandFrontier(row, col) {
 
 
 function addFrontier(row, col) {
-  if (row <= totalRows && row >= 0 && col <= totalColumns && col >= 0) {
+  if (row <= totalRows - 1 && row >= 0 && col <= totalColumns - 1 && col >= 0) {
+    if (grid[row][col] === true) {
+      return;
+    }
     cell = grid[row][col];
     if (!(cell in frontier)) {
       frontier[cell] = [row, col];
@@ -83,25 +96,12 @@ function makeGrid() {
     grid.push([])
     for (var x = 0; x <= width; x += spacing) {
       grid[row].push(col);
-      // draw grid while making it
       fill(50);
       rect(x, y, spacing, spacing);
-      // we might not need this.
-      // it might be best to just use none and true
       col++;
     }
     row++;
   }
-  totalRows = grid.length - 1;
-  totalColumns = grid[0].length - 1;
-}
-
-function Cell() {
-  // the cell's ID
-  this.num = n;
-  // contains the X, Y coordinate on the grid
-  this.row = row;
-  this.col = col;
-  this.marked = false;
-
+  totalRows = grid.length;
+  totalColumns = grid[0].length;
 }
